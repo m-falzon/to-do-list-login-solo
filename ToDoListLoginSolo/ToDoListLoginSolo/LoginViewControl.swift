48e986581c5d8d59792687b37d53e32c5c4d1e85
Matthew Falzon <m.falzon@live.com>
2015-04-29 21:22:11 +1000

    Redid login screen up to same point as in class

--- ToDoListLoginSolo/ToDoListLoginSolo/LoginViewControl.swift
+++ ToDoListLoginSolo/ToDoListLoginSolo/LoginViewControl.swift
@@ -9,3 +9,48 @@
 import Foundation
 
 import UIKit
+
+class LoginViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    override func viewDidLoad() {
+        title = "ToDo App"
+        
+        activityIndicator.stopAnimating()
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        
+        switch(textField) {
+        case emailTextField:
+            emailTextField.resignFirstResponder()
+            passwordTextField.becomeFirstResponder()
+            
+        case passwordTextField:
+            passwordTextField.resignFirstResponder()
+            // Login
+            
+        default:
+            break
+            
+        }
+        
+        return true
+    }
+    
+    @IBAction func onLoginButtonTapped(sender: AnyObject) {
+        doLogin()
+    }
+    
+    func doLogin() {
+        activityIndicator.startAnimating()
+        println("hello")
+    }
+    
+
+}
+
