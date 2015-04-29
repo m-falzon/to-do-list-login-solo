6fe1b26996ce2bcbfea8151f15089f5c4fe34a03
Matthew Falzon <m.falzon@live.com>
2015-04-29 20:45:28 +1000

    Set up auto layout

---
+++ ToDoListLoginSolo/ToDoListLoginSolo/LoginViewControl.swift
@@ -0,0 +1,11 @@
+//
+//  LoginViewControl.swift
+//  ToDoListLoginSolo
+//
+//  Created by Matthew Falzon on 29/04/2015.
+//  Copyright (c) 2015 Matthew Falzon. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
